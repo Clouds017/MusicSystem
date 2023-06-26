fea0eac90d4a40aa5e9f8d43ecc2577ddefaf36b
Clouds017 <1276262968@qq.com>
2023-06-26 23:59:30 +0800

    my

---
+++ MusicSystemApi - idea/src/main/java/com/rabbiter/music/dao/RankMapper.java
@@ -0,0 +1,25 @@
+package com.rabbiter.music.dao;
+
+import com.rabbiter.music.pojo.Rank;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 评价Dao
+ */
+@Repository
+public interface RankMapper {
+    /**
+     *增加
+     */
+    public int insert(Rank rank);
+
+    /**
+     * 查总分
+     */
+    public int selectScoreSum(Integer songListId);
+
+    /**
+     * 查总评分人数
+     */
+    public int selectRankNum(Integer songListId);
+}
---
+++ MusicSystemUserVue/src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Description: 
+ * @Author: Rabbiter
+ * @Date: 2023-03-16 01:26:34
+-->
+<template>
+    <div class="the-footer">
+        <p>Copyright © 忧伤大白兔</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'the-footer'
+}
+</script>
+<style lang="scss" scoped>
+@import '../assets/css/the-footer.scss';
+</style>
